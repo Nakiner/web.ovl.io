502166474f75d85aebb1bdeec1d2781e3f7ea519
Павел Емельянов < >
2016-10-13 14:28:19 +0300

    Фикс багов

--- PHP/pr1/index.php
+++ PHP/pr1/index.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-	
         <title>Практическая работа №1</title>
         <link rel="stylesheet" type="text/css" href="style.css" />
         <meta charset="utf-8" />
     </head>
     <body>
-        <div class="main">
+        <div class="main container">
             <div class="inside-one">
                 <div class="pic"></div>
                 <div class="text">
@@ -27,10 +26,10 @@
 					if(isset($_POST['log']) && $_POST['log'] == $log && $_POST['pwd'] == $pwd) echo 'Привет, '.$_POST['log'];
 					else echo '<form class="form" method="post">
 						<label><b>Имя пользователя</b></label><br />
-						<input type="text" maxlength="30" name="log">
+						<input type="text" maxlength="30" name="log" class="inp">
 						<br /><br />
 						<label><b>Пароль</b></label><br />
-						<input type="password" maxlength="30" name="pwd">
+						<input type="password" maxlength="30" name="pwd" class="inp">
 						<br />
 						<input type="submit" value="Вход" class="submit"/>
 					</form>';
